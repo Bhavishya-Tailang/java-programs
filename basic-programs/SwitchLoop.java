a60536e6b5450d901f42a9c4da1385534171ac92
tailangbhavishya <tailangbhavishya@gmail.com>
2022-11-15 11:38:21 +0530

    added a program to check if day is weekday or weekend using switch

---
+++ basic-programs/SwitchLoop.java
@@ -0,0 +1,32 @@
+/**
+ * program to check if day is weekday or weekend using switch
+ * input:
+ *  user will enter the number of day
+ * ouput:
+ *  result will be given as weekend or weekday
+ */
+
+import java.util.Scanner;
+
+public class SwitchLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the day: ");
+        int day = sc.nextInt();
+        sc.close();
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 6, 7 -> System.out.println("Weekend");
+            default -> System.out.println("Different planet");
+        }
+    }
+}
+
+
+/**
+ * example:
+ * Enter the day: 4
+ *  Weekday
+ * Enter the day: 7
+ *  Weekend
+ */
