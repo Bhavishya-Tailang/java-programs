1f94bdc46e523ee553a31f417ff16b44730910d8
tailangbhavishya <tailangbhavishya@gmail.com>
2022-11-16 10:50:25 +0530

    added program to print string

---
+++ basic-programs/FirstProgram.java
@@ -0,0 +1,14 @@
+/**
+ * program to print string
+ */
+
+class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
+/**
+ * output:
+ * 	Hello World
+ */
